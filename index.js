8cf8ee9aeaab6adc939c87eec7fc593d9847cd48
Harsh Gandhi <harshgandhi043@gmail.com>
2021-08-09 18:27:51 +0530

    initialized project

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const redis = require('redis');
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379
